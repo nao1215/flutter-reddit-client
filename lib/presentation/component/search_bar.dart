9a73f37f7bfec10fcc158c36675aac4a9c3b6910
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2024-11-28 22:46:50 +0900

    Add Home screen
    Delete widget test
    
    Delete test

---
+++ lib/presentation/component/search_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  final Function(String) onSearch;
+
+  const SearchBar({super.key, required this.onSearch});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        onChanged: onSearch,
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          prefixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
